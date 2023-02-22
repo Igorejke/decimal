566c1a39fc3dc2faa00e32b0245b5ffd98231e50
Igorejke <77626541+Igorejke@users.noreply.github.com>
2023-02-22 15:34:29 +0300

    Update test.c
    Test v1.0

--- src/test.c
+++ src/test.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
-int main() {
-  printf("Hello, World!");
+
+int getbits()
+int main()
+{
+    bool a[32];
+    int decimal;
+    int length = 32;
+    int decimal_alt;
+    int count = 0;
+    do {
+        scanf("%d", &decimal);
+        if (decimal < 0) printf("Enter decimal number NON-zero\n");
+    } while (decimal < 0);
+    do {
+        if (decimal % 2 == 0) a[length] = 0;
+        else a[length] = 1;
+        decimal /= 2;
+        length--;
+    } while (decimal != 0);
+    for (int i = 0; i < 33; i++) {
+        printf("%d", a[i]);
+    }
+    printf("\n");
+    for (int i = 32; i > 0; i--) {
+        decimal_alt += pow(2, count) * a[i]; 
+        count++;
+    }
+    printf("%d\n", decimal_alt);
+    return 0;
 }
